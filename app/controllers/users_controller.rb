3d64266663d193aab92b684161f8f31ba7f5b379
takara1356 <goblin7283@gmail.com>
2020-04-01 22:16:22 +0900

    [add] JSON出力成功

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   # GET /users.json
   def index
     @users = User.all
+    render "index", :formats => [:json], :handlers => [:jbuilder]
   end
 
   # GET /users/1
